2348baba95b3db1b5223bf0f84cd0db3d865f934
mirandarevans <miranda.r.evans@gmail.com>
2018-08-23 23:44:51 +0000

    fixing typo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,7 +22,7 @@ def stats():
         returns amount of each type
     '''
     return jsonify(amenities=storage.count('Amenity'),
-                   citites=storage.count('City'),
+                   cites=storage.count('City'),
                    places=storage.count('Place'),
                    reviews=storage.count('Review'),
                    states=storage.count('State'),
