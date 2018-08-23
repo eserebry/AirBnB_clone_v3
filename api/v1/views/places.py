633aa8b91f4e8592cd8ad641f8ff8a00e7aab8d9
mirandarevans <miranda.r.evans@gmail.com>
2018-08-23 16:47:38 +0000

    adding api work so far

---
+++ api/v1/views/places.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from models import storage, Place
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/cities/<city_id>/places', strict_slashes=False,
+                 methods=['GET', 'POST'])
+def places_of_a_city(state_id):
+    '''
+        GET: list all places in a specific city
+        POST: add a place to a specific city
+    '''
+    my_city = storage.get('City', city_id)
+    if my_city is None:
+        return 404
+    if request.method == 'POST':
+        place_dict = request.get_json()
+        if place_dict is None:
+            return 'Not a JSON', 400
+        if 'user_id' not in place_dict.keys():
+            return 'Missing user_id', 400
+        if 'name' not in place_dict.keys():
+            return 'Missing name', 400
+        place_dict['city_id'] = city_id
+        try:
+            my_place = Place(**place_dict)
+            my_place.save()
+        except:
+            return 404
+        return jsonify(my_place.to_dict()), 201
+    my_places = [place.to_dict() for place in storage.all('Place').values()
+                 if place.city_id == city_id]
+    return jsonify(my_places)
+
+
+@app_views.route('/places/<place_id>', strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+def get_place(place_id):
+    '''
+        GET: display a specific place
+        DELETE: delete a place
+        PUT: update a place
+    '''
+    my_place = storage.get('Place', place_id)
+    if my_place is None:
+        return 404
+    if request.method == 'DELETE':
+        storage.delete(my_place)
+        return jsonify({})
+    if request.method == 'PUT':
+        place_dict = request.get_json()
+        if place_dict is None:
+            return 'Not a JSON', 400
+        for key, value in place_dict.items():
+            if key != 'id' and key != 'created_at' and key != 'updated_at':
+                if key != 'user_id' and key != 'city_id':
+                    setattr(my_place, key, value)
+            my_place.save()
+    return jsonify(my_place.to_dict())
---
+++ api/v1/views/states.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os
+from models import storage, State
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from flasgger import swag_from
+
+
+@app_views.route('/states', strict_slashes=False,  methods=['GET', 'POST'])
+@swag_from(os.getcwd() + '/api/v1/views/apidoc/states_get.yaml',
+           methods=['GET'])
+@swag_from(os.getcwd() + '/api/v1/views/apidoc/states_post.yaml',
+           methods=['POST'])
+def all_states():
+    '''
+        GET: list all states
+        POST: create a new state
+    '''
+    if request.method == 'POST':
+        state_dict = request.get_json()
+        if state_dict is None:
+            return 'Not a JSON', 400
+        if 'name' not in state_dict.keys():
+            return 'Missing name', 400
+        my_state = State(**state_dict)
+        my_state.save()
+        return jsonify(my_state.to_dict()), 201
+    my_states = [state.to_dict() for state in storage.all('State').values()]
+    return jsonify(my_states)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+@swag_from(os.getcwd() + '/api/v1/views/apidoc/states_state-id_get.yaml',
+           methods=['GET'])
+@swag_from(os.getcwd() + '/api/v1/views/apidoc/states_state-id_delete.yaml',
+           methods=['DELETE'])
+@swag_from(os.getcwd() + '/api/v1/views/apidoc/states_state-id_put.yaml',
+           methods=['PUT'])
+def get_state(state_id):
+    '''
+        GET: display a specific state
+        DELETE: delete a state
+        PUT: update a state
+    '''
+    my_state = storage.get('State', state_id)
+    if my_state is None:
+        abort(404)
+    if request.method == 'DELETE':
+        storage.delete(my_state)
+        storage.save()
+        return jsonify({})
+    if request.method == 'PUT':
+        state_dict = request.get_json()
+        if state_dict is None:
+            return 'Not a JSON', 400
+        for key, value in state_dict.items():
+            if key != 'id' and key != 'created_at' and key != 'updated_at':
+                setattr(my_state, key, value)
+        my_state.save()
+    return jsonify(my_state.to_dict())
